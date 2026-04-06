584e7c28e2e5c05fc50e9d8b4a55b847e89240cf
imdecentenough <thomas.steel3@outlook.com>
2026-04-06 22:04:37 +1000

    Added Documents re. Projects, and added more stuff to the create account page

--- Marketplace-frontend/src/CreateAccount.jsx
+++ Marketplace-frontend/src/CreateAccount.jsx
@@ -36,20 +36,63 @@ function App() {
   <div id="contentBackground">
     <div id="content">
       <h1> Create Account</h1>
-      <h3>All fields are required. </h3>
-      <div id="createAccountGrid">
-      <p>First Name</p>
-      <p>Surname</p>
-      <p>Username</p>
-      <p>Date of Birth</p>
-      <p>Address</p>
-      <p>Password</p>
-      <p>Confirm Password</p>
-      <p>Terms and Conditions</p>
-      <p>Privacy Policy</p>
-      <p>University Code of Conduct</p>
-      </div>
-      <button class="bigButton"> Finish</button>
+      <h3>All fields are required. </h3><br />
+      <hr /><br />
+        <form class="createAccountForm">
+
+          {/* Given Name (s) */}
+          <label for="givenName">Given Name(s):</label>
+          <input type="text" id="givenName" name="givenName" required/>
+
+          {/* Family Name */}
+          <label for="surname">Family Name:</label>
+          <input type="text" id="surname" name="surname" required/>
+
+          {/* Username */}
+          <label for="username">Username:</label>
+          <input type="text" id="username" name="username" required/><br />
+          <i>This will be your displayed name by default, unless changed otherwise.</i>
+
+          {/* Date of Birth */}
+          <label for="dob">Date of Birth</label>
+          <input type="date" id="dob" name="dob" max="2009-01-01" required/><br />
+          <i>You must be at least 18 years of age to use the Marketplace.</i>
+
+          {/* E-Mail Address */}
+          <label for="email">Email Address:</label>
+          <input type="email" id="email" name="email" required/>
+
+          {/* Password */}
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" minLength="8" required/>
+          <br />
+          <i>Minimum 8 characters.</i>
+
+          {/* Confirm Password */}
+          <label for="passwordConfirm">Confirm Password:</label>
+          <input type="password" id="passwordConfirm" name="passwordConfirm" required/><hr /><br></br>
+          
+          {/* Terms and Conditions */}
+          <label for="terms">Community Marketplace Terms and Conditions Agreement:</label>
+          <input type="checkbox" id="terms" name="terms" value="termsTrue" required></input>
+          <br />
+          <i>T&C: (put link here)</i>
+
+          {/* Privacy Policy */}
+          <label for="privacy">Community Marketplace Privacy Policy Acknowledgement:</label>
+          <input type="checkbox" id="privacy" name="privacy" value="privacyTrue" required></input>
+          <br />
+          <i>Privacy Policy: (put link here)</i>
+
+          {/* University Code of Conduct */}
+          <label for="conduct">University of Marketplace Code of Conduct Agreement:</label>
+          <input type="checkbox" id="conduct" name="conduct" value="conductTrue" required></input>
+          <br />
+          <i>Code of Conduct: (put link here)</i>
+          <input type="submit"></input>
+
+        </form>
+        <hr />
     </div>
   </div>
 </>
