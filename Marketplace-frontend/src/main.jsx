353f4c9e2e1a4174b64dbd223851e2e825629f22
imdecentenough <thomas.steel3@outlook.com>
2026-04-06 18:45:01 +1000

    lil' developer-exclusive css theme :)

--- Marketplace-frontend/src/main.jsx
+++ Marketplace-frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './css/index.css'
+import './css/frutiger.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home.jsx'
 import Login from './Login.jsx'
--- draftfiles/script.js
+++
@@ -1,8 +0,0 @@
-function handleSearch(event) {
-    if (event.key === 'Enter') {
-        const query = event.target.value.trim();
-        if (query) {
-            window.location.href = '/search?=' + encodeURIComponent(query);
-        }
-    }
-}
