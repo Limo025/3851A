067d45820c6be648a34d8b22f29fbb976806fe91
imdecentenough <thomas.steel3@outlook.com>
2026-04-10 09:02:09 +1000

    css change

--- Marketplace-frontend/src/main.jsx
+++ Marketplace-frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './css/frutiger.css'
+import './css/index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
