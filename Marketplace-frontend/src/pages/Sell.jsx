3515ddf1ff263df80ed15f20b47d55535f91686a
imdecentenough <thomas.steel3@outlook.com>
2026-09-04 11:44:26 +1000

    basic outline for Sell page

--- Marketplace-frontend/src/pages/Sell.jsx
+++ Marketplace-frontend/src/pages/Sell.jsx
@@ -35,6 +35,37 @@ function App() {
   <div id="contentBackground">
     <div id="content">
       <h1> Sell </h1>
+      {/*       
+      Information for the user to input: 
+      - account details are autoprovided via account - if the user is not signed in, redirect them to login page. 
+      - *Name of item
+      - *price
+      - *description
+      - *image(s)
+      - tags
+      
+      */}
+      <div class="ItemForm">
+
+          <label for="itemNameForm">Name of Item:  </label>
+          <input required placeholder="The name of your item; for customers to identity." name="itemNameForm" id='itemNameForm'/>
+
+          <label for="itemPriceForm">Price (in Australian Dollars)</label>
+          <input required type="number" name="itemPriceForm" id="itemPriceForm" placeholder="$"></input>
+          
+          <label for="itemDescription">Description:  </label>
+          <textarea class="itemDescription" id="itemDescription" name="itemDescription" placeholder="A simple description for users to know anything about the item, such as any faults, irregularities, or features, as well as information regarding collection of item (eg. Delivery / Pickup only, or whether negotiation is allowed" required></textarea>
+
+          <label for="itemImageFile">Upload Image(s)</label>
+          <input class="uploadButton" type="file" id="itemImageFile" name="itemImageFile" placeholder="Choose Files" multiple accept="image/png, image/jpeg, image/jpg" />
+
+          <label for="itemDescription">Tags:  </label>
+          <textarea class="itemDescription" id="itemDescription" name="itemDescription" placeholder="This helps customers find the item better through searching and looking at similar items such as this. 
+          
+          Enter a tag in lowercase, followed by ';', and additional tags you wish to add. For example, a television would have tags such as 'television; tv; 4k; 50inch; sony;  etc. ' " ></textarea>
+
+        <button class="bigButton" >Submit</button>
+      </div>
     </div>
   </div>
 </>
