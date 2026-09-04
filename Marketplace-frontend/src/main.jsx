93f9787ceee0da363e84808ea7f0ee4343ed300a
Tann03519 <tann03519@gmail.com>
2026-09-04 12:38:43 +1000

    fix: restore frontend shell and lock helmet dependency

--- Marketplace-frontend/src/main.jsx
+++ Marketplace-frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './css/modern.css'
+import './css/index.css'
 import './js/script.js'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
